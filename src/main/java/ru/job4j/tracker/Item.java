fa724382ce73b654ccf5b482079e40ce6378f040
ubatyr <ubatyr@mail.ru>
2022-03-23 14:20:06 +0500

    2. Перегрузка конструктора [#173890 #277765]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,6 +4,22 @@ public class Item {
     private int id;
     private String name;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId() {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public Item() {
     }
 
