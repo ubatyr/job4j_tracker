e68b880a31819f47ade744da7e9a93207617a90a
ubatyr <ubatyr@mail.ru>
2022-03-31 12:00:44 +0500

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -10,5 +10,6 @@ public class StartUI {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String dateTimeFormat = item.getCreated().format(formatter);
         System.out.println(dateTimeFormat);
+        System.out.println(item);
     }
 }
