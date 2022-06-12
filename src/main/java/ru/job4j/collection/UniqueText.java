7e57596d30e109fe5e08464bfba66bcfd7bff93e
ubatyr <ubatyr@mail.ru>
2022-06-12 09:48:44 +0500

    2. Идентичные тексты [#212655 #305119]

--- src/main/java/ru/job4j/collection/UniqueText.java
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -14,6 +14,7 @@ public class UniqueText {
         for (String wordText : text) {
             if (!check.contains(wordText)) {
                 rsl = false;
+                break;
             }
         }
         return rsl;
