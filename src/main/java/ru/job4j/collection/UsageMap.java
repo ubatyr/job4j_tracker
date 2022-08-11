85ed6253f48c737922eb1faa048acd631e05fe19
ubatyr <ubatyr@mail.ru>
2022-08-11 13:52:23 +0500

    0. Map, HashMap [#212652 #319304]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -5,7 +5,9 @@ import java.util.HashMap;
 public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
-        map.put("Ubatyr@mail.ru", "Bobozhonov Batyr Uktamovich");
+        map.put("ubatyr@mail.ru", "Bobozhonov Batyr Uktamovich");
+        map.put("parsentev@yandex.ru", "Arsentev Petr");
+        map.put("ubatyr@mail.ru", "Bobozhonov Batyr");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println((key + " = " + value));
